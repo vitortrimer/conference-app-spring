1bcb16550043e6715571fdc3a2adc53e2f199da4
Vitor Trimer <vitortrimer@gmail.com>
2021-05-10 19:52:55 -0300

    Add project basis

---
+++ src/test/java/trimer/vitor/conferencedemo/ConferenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package trimer.vitor.conferencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
